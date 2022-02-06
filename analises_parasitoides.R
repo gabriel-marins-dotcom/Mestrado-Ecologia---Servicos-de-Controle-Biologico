682716c6a1806eea1a45214677cb7f5f25f94461
unknown <g.marins.rrf@gmail.com>
2022-02-06 00:01:08 -0300

    Commit inicial do script

---
+++ analises_parasitoides.R
@@ -0,0 +1,8 @@
+#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#
+#  Análises Serviços de Controle      #
+#  Biológico de Parasitóides          #
+#                                     #
+#  Gabriel Marins, Pedro Togni, Raul  #
+#  Laumann,                           #
+#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#¨#
+
